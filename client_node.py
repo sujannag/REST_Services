74df60de02a28d41ee2100eb299771b5b922dc62
Sujan Nag <nags@tcd.ie>
2017-12-13 09:26:49 +0000

    able to get files from the git

--- client_node.py
+++ client_node.py
@@ -1,19 +1,94 @@
 #client_node.py
 
 import requests
+import os
+#import lizard
 
+
+MASTER_URL = 'http://0.0.0.0:5000/'
+NODE_SETUP_URL = 'http://0.0.0.0:5000/init'
 
 class client_node:
 
-	master_url = 'http://0.0.0.0:5000/'
-	#node_setup_url = 
-	
+	global MASTER_URL
+	global NODE_SETUP_URL
+
 	def __init__(self):
-		#self.value = requests.get(self.master_url).json()['url']
-		self.value = requests.get(self.master_url).json()
-		print self.value
+		self.files_list_url = requests.get(NODE_SETUP_URL).json()['url']
+		print(self.files_list_url)
+
+	def compute_cc(self):
+		self.sha = self.get_job()
+		if self.sha is None:
+			print ("Empty Sha")
+			#continue
+
+		file_tree = self.get_file_tree(self.sha)
+		#print (file_tree)
+		
+		self.get_files(file_tree)
+		avg_cc = self.get_average_cc()
+		
+
+	def get_job(self):
+		'''
+		Get jobs from the master node
+		'''
+		
+		resp = requests.get(MASTER_URL)
+		print (resp.status_code)
+		
+		if(resp.status_code == 200):
+			return resp.json()['sha']
+
+	def do_job(self):
+		'''
+		Calculate the cyclomatic complexity
+		'''
+		pass
+
+	def get_file_tree(self, sha):
+		#print type(self.files_list_url)
+			
+		resp = requests.get(self.files_list_url.format(sha))
+		if resp.status_code == 301:
+			redirect_url = resp.headers['location'].split('?')[0]
+			resp = requests.get(redirect_url)
+		return resp.json()['tree']
+		
+
+	def get_files(self, file_tree):
+	
+		blob_urls = []
+		
+		if(True != os.path.isdir('tmp')):
+			print("Creating Temp Directory")
+			os.makedirs('tmp')
+		else:
+			print("Temp Directory already present!!")
+		
+		for item in file_tree:
+			#if item['type'] == 'blob' and self.is_py_file(item['path']):
+			if item['type'] == 'blob':
+				blob_urls.append(item['url'])
+
+	    #headers = {'Accept': 'application/vnd.github.v3.raw'}
+		#TODO: Try to change this logic	
+		for index, url in enumerate(blob_urls):
+			#print (index, url)
+			resp = requests.get(url)
+			with open('./tmp/{}.py'.format(index), 'w') as tmp_file:
+				tmp_file.write(resp.text)
+		
+	def get_average_cc(self):
+		
+		i = lizard.analyze_file('./tmp')
+		print("CC",i.average_cyclomatic_complexity)
 
 if __name__ == '__main__':
 	client = client_node()
+	
+	# Get the jobs fr
+	client.compute_cc()
 
 
--- master_node.py
+++ master_node.py
@@ -1,6 +1,27 @@
 #import web #similar to flask. use any of the two
 from flask import Flask
 from flask_restful import Resource, Api, request
+from collections import deque
+from time import time
+#from git import Repo
+import requests
+from threading import Lock
+
+#GIT_REPO_PATH = "git_repo/pacman"
+GIT_REPO_PATH = 'https://api.github.com/repos/weixsong/pacman/commits'
+GIT_REPO_FILES_PATH = 'https://api.github.com/repos/weixsong/pacman/git/trees/{}'
+#GIT_REPO_PATH = 'https://api.github.com/repos/sujannag/scalable_computing/commits'
+
+g_total_no_of_commits = 0
+g_clients_connected_count = 0
+
+# define queue for storing the jobs that needs to be done by the clients
+job_queue = deque()
+
+# Lock for the job queue
+job_queue_lock = Lock()
+clients_connected_count_lock = Lock()
+cc_lock = Lock()
 
 
 app = Flask(__name__)
@@ -9,35 +30,124 @@ api = Api(app)
 '''
 '''
 class master_node(Resource):
+	
 	def get(self):
-		return 'Hello!!'
+		#return 'Hello!!'
+		'''
+		Give jobs to the client nodes till there are no more jobs to give
+		There can be multiple client nodes asking for Jobs. Hence implement
+		locks on the job_queue
+		'''
+		global job_queue
+		global job_queue_lock
+		
+		print ("Sending jobs!!")
+		with job_queue_lock:
+			try:
+				return{'sha': job_queue.popleft()}
+
+			except IndexError:
+				# TODO: Check out the error codes
+				return '', 204
 
 	def put(self):
-		pass
 		
+		'''
+		get the calculated Cyclomatic complexity from the client nodes 
+		and store it in a list
+		'''
+		cc_value = float(requests.form['cc'])
+		
+		with cc_lock:
+			cyclomatic_complexity += cc_value
+			cc_count += 1
 
+			'''
+			Check if all the commits have been exhausted, if yes shutdown the master node.
+			'''
+			if(cc_count == g_total_no_of_commits):
+					shutdown_master()
+					return '', 503
+			return '', 204
+
+class setup_node(Resource):
+	
+	def get(self):
+		'''
+		Maintain the count of clients connecting to the master node
+		'''
+		global GIT_REPO_FILES_PATH
+		global g_clients_connected_count
+		global clients_connected_count_lock
+		
+		with clients_connected_count_lock:
+			g_clients_connected_count += 1
+		
+		print("New Client Connected!!", g_clients_connected_count)
+		return {'url': GIT_REPO_FILES_PATH}
+		
+	def put(self):
+		pass
 
 def get_commits():
 	# get the commit files from the github
 	print ("Getting commits from Github")
 
+	global GIT_REPO_PATH
+	global job_queue
+	
+	resp = requests.get(GIT_REPO_PATH)
+	#print resp
+
+	while 'next' in resp.links:
+		for item in resp.json():
+			job_queue.append(item['sha'])
+		resp = requests.get(resp.links['next']['url'])
+	#print(resp.json())
+
+	# store the commits in a queue
+	for item in resp.json():
+		job_queue.append(item['sha'])
+
+
+	#repo = Repo(GIT_REPO_PATH)
+	#print repo
+	#sha = repo.head.reference.commit.hexsha
+	
+	
+
+def calculate_average_cc():
+	print("Average CC")
+
+def plot_graph():
+	print("Plot Graph")
+
+def shutdown_master():
+	print "shutdown master"
 
 api.add_resource(master_node, '/')
+api.add_resource(setup_node, '/init')
 
 if __name__ == '__main__':
 	# Get the commit files from the git
 	get_commits()
 
 	# get the total number of commits
+	g_total_no_of_commits = len(job_queue)
+	print ('total_no_of_commits: ', g_total_no_of_commits)
 
 	# Start the time
+	start_time = time()
 
 	# Start assigning tasks to the workers
 	app.run(host='0.0.0.0', port=5000, debug=False)
 
 	# end the time
+	end_time = time()
 
-	# Calculate the time
+	# Calculate the total time required
+	delta_time = end_time - start_time
+	print delta_time
 
 	# Plot the graphs
 
